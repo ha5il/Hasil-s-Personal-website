f6e90fd85c53703b1fe439e0636baac79524cb0c
Hášíl Páůďýál <Hulsha@Hulshas-MacBook-Pro.local>
2016-01-22 00:53:08 +0530

    :)

---
+++ init.js
@@ -0,0 +1,12 @@
+//side navigation
+$('.button-collapse').sideNav({
+  menuWidth: 190,
+  edge: 'left',
+  closeOnClick: true
+});
+
+//preloader
+$(window).load(function() {
+  $(".myloader").delay(100).fadeOut(1400)
+});
+
