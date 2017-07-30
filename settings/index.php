58964033f152ccd48b60d4cd86605b9360bbdf7b
Hášíl Páůďýál <ha5il@users.noreply.github.com>
2017-07-30 19:59:34 +0530

    theme and lang choose page

---
+++ settings/index.php
@@ -0,0 +1,37 @@
+<html>
+   <head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+   </head>
+   <body>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('en')">English</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('np')">Nepali</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('in_hi')">Hindi</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('in_ta')">Tamil</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('in_tel')">Telugu</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('de')">German</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('fr')">French</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('es')">Spanish</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('it')">Italian</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('nl')">Dutch</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('tr')">Turkish</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('pl')">Polish</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('gr')">Greek</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('pt')">Portugese</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('hr')">Croatian</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('al')">Albanian</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('rs')">Serbian</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('bg')">Bulgarian</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('ro')">Romanian</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('hu')">Hungarian</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('no')">Norwegian</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('ir')">Persian</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('jp')">Japanese</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('vn')">Vietnamese</button>
+      <button class="btn blue waves-effect waves-green" onClick="cookie_lang('cz')">Czech</button>
+      <input id="background-color" type="color" value="#ff0000" onchange="javascript:get_theme_colour(document.getElementById('background-color').value);">
+      <button class="btn blue waves-effect waves-green" onClick="set_theme_colour('random')">Random Colour</button>
+      <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+      <script src="/allassets/js/cookie.js"></script>
+   </body>
+</html>
