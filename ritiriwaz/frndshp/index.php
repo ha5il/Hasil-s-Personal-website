ea4863a65cd1f38dcf5535e54a5fa3ae901f469b
Hášíl Páůďýál <ha5il@users.noreply.github.com>
2017-08-08 18:43:24 +0530

    fixed title

--- ritiriwaz/frndshp/index.php
+++ ritiriwaz/frndshp/index.php
@@ -10,7 +10,7 @@
 <link href="/allassets/icon.png" rel="shortcut icon">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
 <style rel="stylesheet" type="text/css" media="screen,projection">nav{font-family:'Kaushan Script',serif;<?php echo 'background:#'."$design"; ?>}@font-face{font-family:'Kaushan Script';font-style:normal;font-weight:400;src:local('Kaushan Script'),local('KaushanScript-Regular'),url(https://fonts.gstatic.com/s/kaushanscript/v5/qx1LSqts-NtiKcLw4N03IO87R-l0-Xx_7cYc0ZX1ifE.woff2) format('woff2');unicode-range:U+0100-024F,U+1E00-1EFF,U+20A0-20AB,U+20AD-20CF,U+2C60-2C7F,U+A720-A7FF}@font-face{font-family:'Kaushan Script';font-style:normal;font-weight:400;src:local('Kaushan Script'),local('KaushanScript-Regular'),url(https://fonts.gstatic.com/s/kaushanscript/v5/qx1LSqts-NtiKcLw4N03IEd0sm1ffa_JvZxsF_BEwQk.woff2) format('woff2');unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2212,U+2215,U+E0FF,U+EFFD,U+F000}.myloader{background:url(/allassets/preloader.gif) center center no-repeat <?php echo "#"."$design"; ?>;bottom:0;height:100%;left:0;overflow:hidden;position:absolute;right:0;top:0;width:100%;z-index:99999}footer{font-family:'Kaushan Script',serif;<?php echo 'background:#'."$design"; ?>;  position: fixed;right: 0;bottom: 0;left: 0;}body{background-color:#adbdf3}.container{font-family:'Kaushan Script'}</style>
-<title>Hasil Wishes you a Happy Ugadi</title>
+<title>Hasil Wishes you Happy Friendship Day</title>
 </head>
 <body>
 <div class="myloader"></div>
