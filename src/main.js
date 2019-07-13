1b9f05e30b2134b304bb33f672f9dc238f236b02
Hášíl Páůďýál <hasilpaudyal@gmail.com>
2019-07-13 16:02:43 +0545

    google analytics added

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
 
@@ -11,3 +12,8 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(VueAnalytics, {
+  id: 'UA-72808058-1',
+  checkDuplicatedScript: true
+})
