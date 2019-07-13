6305866242d07a7c931e0a25c97ac928c77962f3
Hášíl Páůďýál <hasilpaudyal@gmail.com>
2019-07-13 15:48:46 +0545

    homepage design added

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,53 @@
 <template>
 <div id="app">
-    <div id="nav">
-        <b-navbar toggleable="lg" type="dark" variant="info">
-            <router-link tag="b-navbar-brand" :to="{name:'home'}">Hasil Paudyal</router-link>
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-            <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav class="ml-auto">
-                    <router-link tag="b-nav-item" :to="{name:'about'}">About</router-link>
-                    <router-link tag="b-nav-item" :to="{name:'project',slug:'all'}">Projects</router-link>
-                </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
-    </div>
-    <router-view />
+    <b-container fluid>
+    <b-row class="pt-2">
+        <b-col cols="4" class="ml-2">
+            <b-card class="card-poem p-5" img-src="/Hasil.png" img-alt="Hasil Paudyal" img-top>
+                <b-card-text class="card-text-poem">
+                    <strong>H</strong>ere I am<br><strong>A</strong> cut above you all<br><strong>S</strong>ilent as a grave<br><strong>I</strong>n favour of your death<br><br><strong>L</strong>ethal is my pen<br><strong>P</strong>erfect would be again<br><strong>A</strong> fortune is with me<br><strong>U</strong>nbeatable this shall be<br><br><strong>D</strong>are you come near<br><strong>Y</strong>ell you out there<br><strong>A</strong>nd who is this me<br><strong>L</strong>inks the first letters to me </b-card-text>
+            </b-card>
+        </b-col>
+        <b-col>
+              <b-img class="pb-3" src="/cover.jpg" rounded fluid alt="Hasil Cover"></b-img>
+                <b-nav align="right" small>
+                    <b-nav-item><b-link to="/">Projects</b-link></b-nav-item>
+                    <b-nav-item><b-link to="/">Quotes</b-link></b-nav-item>
+                    <b-nav-item><b-link to="/">Hire Me</b-link></b-nav-item>
+                    <b-nav-item><b-link to="/">Get in Touch</b-link></b-nav-item>
+                </b-nav>
+            <router-view />
+        </b-col>
+    </b-row>
+    </b-container>
 </div>
 </template>
 
 <style>
-#nav .navbar-brand {
-    cursor: pointer;
+body {
+    font-family: arial;
+}
+.nav {
+    background-color: #AC252D;
+    color: #FFFFFF;
+    height: 35px;
+}
+.nav-item a{
+    color: #ffffff;
+}
+.card-poem{
+    background-color: #FBF1FF;
+    border: none;
+    border-radius: 20px;
+    padding-top: 15px;
+    padding-bottom: 15px;
+}
+.card-text-poem {
+    text-align: center;
+    letter-spacing: 1px;
+    font-size: 16px;
+    line-height: 20px;
+    margin: 20px auto;
+    color: #6E161C;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,69 @@
 <template>
-  <div class="section">
-      <template>
-      <h1 class="title">Hasil Paudyal</h1>
-    </template>
-  </div>
+<div id="homepage" class="section">
+    <b-row class="pt-3">
+        <b-col>
+            <b-card class="mb-3" title="Who am I?">
+                <b-card-text class="justified">
+                    Not always starting from A ends you in Z, sometimes you have to continue with AA and create any word of your own. Though having Graduation of Electrical Engineering I do have skills in Design, Web Development, Electrical repair and Electronics simulation. Out of the box, I write poems, speak quotes and try altering cloud saved data.
+                </b-card-text>
+            </b-card>
+            <b-card title="What I do?">
+                <b-card-text>
+                    <span class="text-info">
+                        Web Developer at View9 | Nepal<br>
+                        (Jan 2019 - Present)<br>
+                    </span>
+                    I develop the backend of website and connect its database to different platforms with api.<br>
+                    <br>
+                    <span class="text-info">
+                        Juniour Android Developer at Fixolla | India<br>
+                        (Feb 2018 - Dec 2018)<br>
+                    </span>
+                    I was not only the part of android development team but also developed api services for the website.<br>
+                    <br>
+                    <span class="text-info">
+                        Security Analyst at Fixolla | India<br>
+                        (Sep 2017 - Feb 2018)<br>
+                    </span>
+                    I helped the company in building their database and securing it from different threats.<br>
+                    <br>
+                    <span class="text-info">
+                        Service and Sales Engineer at A2Z Laundry Solutions | India<br>
+                        (Dec 2016 - Dec 2018)<br>
+                    </span>
+                    I was involved in laundry business setup, marketing and it growth for dorzens of laundries in India. I am one of the authorised service Engineer for Danube International (France), Pony(Italy) and SEKO (USA). Beside these, I am involved in other brands like Electrolux (Sweden), Maytag (USA), Speedqueen (USA), Hydrosystems (USA), SEITZ (Germany) and Sunrise (India).
+                </b-card-text>
+            </b-card>
+        </b-col>
+        <b-col>
+          <b-img class="pb-3" src="/timeline.png" fluid alt="Hasil Timeline"></b-img>
+        </b-col>
+    </b-row>
+</div>
 </template>
 
-<script>
-export default {
-  //
-};
-</script>
+<style lang="scss">
+#homepage {
+    .card {
+        background-color: #FBF1FF;
+        border: none;
+    }
+
+    .card-title {
+        color: #6E161C;
+    }
+
+    .card-text {
+        color: #6E161C;
+
+        span {
+            font-size: 12px;
+            font-weight: 500;
+        }
+    }
+
+    .justified {
+        text-align: justify;
+    }
+}
+</style>
