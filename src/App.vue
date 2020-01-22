af7644a0caf657a8d1aa385cfe1dad0a5f7876ff
Hasil Paudyal <hasilpaudyal@gmail.com>
2020-01-22 20:38:11 +0545

    added background animation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <div class="bg"></div><div class="bg bg2"></div><div class="bg bg3"></div>
     <b-container fluid v-on:mousemove="resetTheme">
       <b-row class="pt-2">
         <b-col
@@ -170,6 +171,37 @@
 }
 .fade-leave-active {
   opacity: 0;
+}
+
+.bg {
+	animation: slide 3s ease-in-out infinite alternate;
+	background-image: linear-gradient(-60deg, var(--global-primary-color) 50%, var(--global-secondary-color) 50%);
+  opacity: 0.4;
+	bottom: 0;
+	left: -50%;
+	opacity: .5;
+	position: fixed;
+	right: -50%;
+	top: 0;
+	z-index: -1
+}
+
+.bg2 {
+	animation-direction: alternate-reverse;
+	animation-duration: 4s
+}
+
+.bg3 {
+	animation-duration: 5s
+}
+
+@keyframes slide {
+	0% {
+		transform: translateX(-25%)
+	}
+	100% {
+		transform: translateX(25%)
+	}
 }
 
 .nav {
